d7f7735df2467099213e6f5304447697e8cdd4ac
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2020-03-16 02:58:52 -0700

    Create checkboxes.dart

---
+++ lib/checkboxes.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxInListview extends StatefulWidget {
+  @override
+  _CheckBoxInListviewState createState() => _CheckBoxInListviewState();
+}
+
+class _CheckBoxInListviewState extends State<CheckBoxInListview> {
+  bool _isChecked = true;
+
+  List<String> _texts = [
+    "Movies",
+    "Music",
+    "Exercise",
+    "Games"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select likes to proceed with registration"),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(8.0),
+        children: _texts.map((text) => CheckboxListTile(
+          title: Text(text),
+                        value: _isChecked,
+                        onChanged: (val) {
+                          setState(() {
+                            _isChecked = val;
+                            
+                          });
+                        },
+
+
+        )).toList(),
+        
+      ),
+    );
+  }
+}
