6acfad93297c46657ee1823dfc4420323e7e09d6
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2020-03-16 05:42:00 -0700

    Update checkboxes.dart

--- lib/Logins/checkboxes.dart
+++ lib/Logins/checkboxes.dart
@@ -45,6 +45,7 @@ class _CheckBoxInListviewState extends State<CheckBoxInListview> {
 
     
       ),
+      floatingActionButton: RaisedButton(onPressed: (){}),
     
     );
 
@@ -57,37 +58,3 @@ class _CheckBoxInListviewState extends State<CheckBoxInListview> {
 
 
 
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const SizedBox(height: 30),
-          RaisedButton(
-            onPressed: () {},
-            textColor: Colors.white,
-            padding: const EdgeInsets.all(0.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Color(0xFF0D47A1),
-                    Color(0xFF1976D2),
-                    Color(0xFF42A5F5),
-                  ],
-                ),
-              ),
-              padding: const EdgeInsets.all(10.0),
-              child:
-                  const Text('Register', style: TextStyle(fontSize: 20)),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
