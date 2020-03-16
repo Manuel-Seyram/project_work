cbe4cb87ed1f6d0f7b7d37ac1e06eb4cb271e7a7
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2020-03-16 04:21:39 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import  'Logins/checkboxes.dart';
 
 
 void main(List<String>args){
@@ -137,71 +138,4 @@ class _SignUpState extends State <SignUp> {
 
   
 
-}
-
-
-
-//new page to checkbox
-
-class CheckBoxInListview extends StatefulWidget {
-  @override
-  _CheckBoxInListviewState createState() => _CheckBoxInListviewState();
-}
-
-class _CheckBoxInListviewState extends State<CheckBoxInListview> {
-  bool _isChecked = true;
-
-  List<String> _texts = [
-    "Movies",
-    "Music",
-    "Exercise",
-    "Games"
-  ];
-  @override
-  Widget build(BuildContext context) {
-
-    final proceedButton = Material(
-
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Colors.blue,
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: (){
-          
-        },
-        child: Text("Register", textAlign:TextAlign.center , style: style.copyWith(color: Colors.white, fontWeight: FontWeight.bold)), // text
-      ),
-
-    );
-
-
-
-
-
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Select likes to proceed with registration"),
-        backgroundColor: Colors.blue,
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(8.0),
-        children: _texts.map((text) => CheckboxListTile(
-          title: Text(text),
-                        value: _isChecked,
-                        onChanged: (val) {
-                          setState(() {
-                            _isChecked = val;
-                            
-                          });
-                        },
-
-
-        )).toList(),
-        
-      ),
-    );
-  }
 }
