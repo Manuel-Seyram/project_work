9fe76c57e632172b44be6d6f33cb9ad59d403200
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2020-04-15 20:44:16 -0700

    Update splash.dart

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,65 +1,83 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:project_work/loginscreens.dart';
+import 'navigation.dart';
 
 
 class SplashScreen extends StatefulWidget {
   @override
-  State <StatefulWidget> createState() {
-    return SplashState();
-  }
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class SplashState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    startTime();
+    Timer(Duration(seconds: 10), () => MyNavigator.goToIntro(context));
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: initScreen(context),
-    );
-  }
-  
-  startTime() async {
-    var duration = new Duration(seconds: 6);
-    return new Timer(duration, route);
-  }
-route() {
-    Navigator.pushReplacement(context, MaterialPageRoute(
-        builder: (context) => Loginscreens()
-      )
-    ); 
-  }
-  
-  initScreen(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Image.asset("assets/logo/alleviate logo.png"),
-            ),
-            Padding(padding: EdgeInsets.only(top: 20.0)),
-            Text(
-              "Alleviate",
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.white
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.blueAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[ 
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 60.0,
+                        backgroundImage: AssetImage('assets/images/alleviatelogo.jpg')
+                        //backgroundColor: Colors.transparent,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Alleviate",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-            Padding(padding: EdgeInsets.only(top: 20.0)),
-            CircularProgressIndicator(
-              backgroundColor: Colors.white,
-              strokeWidth: 1,
-           )
-         ],
-       ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(backgroundColor: Colors.white,),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "Smile Again",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
