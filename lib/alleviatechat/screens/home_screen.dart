1358551e2d2ecd866e44fb1e66df131bd3709920
Manuel-Seyram <51480103+Manuel-Seyram@users.noreply.github.com>
2020-07-07 06:47:40 -0700

    Update home_screen.dart

--- lib/alleviatechat/screens/home_screen.dart
+++ lib/alleviatechat/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ui/widgets/category_selector.dart';
-import 'package:flutter_chat_ui/widgets/favorite_contacts.dart';
-import 'package:flutter_chat_ui/widgets/recent_chats.dart';
+import 'package:project_work/alleviatechat/widgets/category_selector.dart';
+import 'package:project_work/alleviatechat/widgets/favorite_contacts.dart';
+import 'package:project_work/alleviatechat/widgets/recent_chats.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -51,7 +51,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               child: Column(
                 children: <Widget>[
-                  FavoriteContacts(),
                   RecentChats(),
                 ],
               ),
